392876ef66039f6a3579f6070d9ee7769692dcac
ljcucc <ljcucc@gmail.com>
2023-07-14 10:46:52 +0800

    first commit

---
+++ bin/irc.dart
@@ -0,0 +1,5 @@
+import 'package:irc/irc.dart' as irc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${irc.calculate()}!');
+}
---
+++ test/irc_test.dart
@@ -0,0 +1,8 @@
+import 'package:irc/irc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
